587e9c439acf66c6dfa5e9f56fe6320b48564591
ClementDescamps <clementdescamps59@gmail.com>
2017-03-27 17:00:26 +0200

    Add files via upload

--- splash/splash/fonctions/gestion_inscritption.php
+++ splash/splash/fonctions/gestion_inscritption.php
@@ -1,3 +1,4 @@
+<meta charset="utf-8"/>
 <?php
 	require ('connectBD.php');
 
@@ -9,23 +10,31 @@
 		$pseudo = $_POST['username'];
 		$mdp = $POST['password'];
 		$mdp2 = $POST['password2'];
-		$dateNaiss = $POST['birthdate'];
-		$email = $POST['email'];
+		//$dateNaiss = $POST['birthdate'];
+		//$email = $POST['email'];
 		
 		echo $_POST['lastname'];
 		echo $_POST['firstname'];
 		echo $_POST['username'];
 		echo $_POST['password'];
-		echo $_POST['passxord2'];
-		echo $_POST['birthdate'];
-		echo $_POST['email'];
+		echo $_POST['password2'];
+		//echo $_POST['birthdate'];
+		//echo $_POST['email'];
 		
 		//TO DO tests si données valides et cohérentes
+		if ($_POST['password'] <> $_POST['password2']) {
+			echo 'mots de passe ne correspondent pas';
+		} else {
+			//inscription dans la base de données
+			$motDePasse = $_POST['password'];
+			global $connexion;
+			$var = $connexion->prepare("INSERT INTO UserProfil (nom, prenom, pseudo, mdp) values ('$nom', '$prenom', '$pseudo', '$motDePasse')");
+			$var->execute();
+		}
 		
-		//inscription dans la base de données
-		global $connexion;
-		$var = $connexion->prepare("INSERT INTO UserProfil (nom, prenom, pseudo, mdp, dateNaiss, email) values ('$nom', '$prenom', '$pseudo', '$mdp', '$dateNaiss', '$email')");
-		$var->execute();
 		
+		
+	} else {
+		Header('Location: ../Accueil.php');
 	}
 ?>
