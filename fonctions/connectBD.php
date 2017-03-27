b36fce69ed973c5250eaa38fe25d34aceca92f13
ClementDescamps <clementdescamps59@gmail.com>
2017-03-27 14:29:08 +0200

    Delete connectBD.php

--- fonctions/connectBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-	try{
-		$connexion = new PDO("mysql:host=localhost;dbname=lalande","lalande","cocotioten");
-	}
-	catch(PDOException $e){
-		echo("Erreur connexion : ".$e->getMessage());
-		exit();
-	}
-?>
