b4273d35ae034f344442f810b8b3ed3c90c89117
LucasBourre <bourre.lucas@gmail.com>
2017-04-19 14:45:01 +0200

    modbdd

--- splash/splash/fonctions/connectBD.php
+++ splash/splash/fonctions/connectBD.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$connexion = new PDO("mysql:host=localhost;dbname=lalande","lalande","cocotioten");
+		$connexion = new PDO("mysql:host=mysql.hostinger.fr;dbname=u748541759_lala","u748541759_lala","cocotioten");
 	}
 	catch(PDOException $e){
 		echo("Erreur connexion : ".$e->getMessage());
