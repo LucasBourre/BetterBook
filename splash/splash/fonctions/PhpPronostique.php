9ef1ccbbb09808759e48783d18b2f8db38edd16e
Bilel1993 <alouibilel93@yahoo.fr>
2017-05-14 19:28:58 +0200

    Update PhpPronostique.php

--- splash/splash/fonctions/PhpPronostique.php
+++ splash/splash/fonctions/PhpPronostique.php
@@ -33,7 +33,7 @@
 					    	 	<button class="bouton" value=<?php echo $donnees['cote1']; ?>> <?php echo $donnees['cote1']; ?> </button>
 					    	 </span>
 					    	 
-					    	 <span class ="equipe" value="N"> match null  
+					    	 <span class ="equipe" value="N"> match nul 
 								<button class="boutonX" value=<?php echo $donnees['coteN']; ?>> <?php echo $donnees['coteN']; ?></button>
 							</span>
 							
